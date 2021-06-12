6ebb85a9acb2cf2ab66a7958478ee9c7430ae857
soaza <65228562+soaza@users.noreply.github.com>
2021-06-12 12:30:31 +0800

    change url

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,11 @@ const App: FC = () => {
 
       <div className="App">
         <Switch location={location} key={location.pathname}>
-          <Route exact path="/" component={MainPage}></Route>
-          <Route exact path="/dogs" component={DogPage}></Route>
-          <Route path="/exco" component={ExcoPage}></Route>
-          <Route path="/activity" component={ActivityPage}></Route>
-          <Route path="/contact" component={ContactPage}></Route>
+          <Route exact path="/Pawfriends/" component={MainPage}></Route>
+          <Route exact path="/Pawfriends/dogs" component={DogPage}></Route>
+          <Route path="/Pawfriends/exco" component={ExcoPage}></Route>
+          <Route path="/Pawfriends/activity" component={ActivityPage}></Route>
+          <Route path="/Pawfriends/contact" component={ContactPage}></Route>
         </Switch>
       </div>
     </>
