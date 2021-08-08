6ed7f05bff7c064b64fc8ddc170065f5e35b51e5
soaza <65228562+soaza@users.noreply.github.com>
2021-08-08 18:45:54 +0800

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { FC } from "react";
 import "./index.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import DefaultLayout from "./components/Layout";
@@ -9,24 +8,22 @@ import MainPage from "./pages/MainPage/MainPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
 import AboutUsPage from "./pages/AboutUsPage/AboutUsPage";
 
-const App: FC = () => {
+const App = () => {
   return (
-    <>
-      <Router>
-        <div className="App">
-          <DefaultLayout />
+    <Router>
+      <div className="App">
+        <DefaultLayout />
 
-          <Switch>
-            <Route exact path="/dogs" component={DogPage}></Route>
-            <Route exact path="/exco" component={ExcoPage}></Route>
-            <Route exact path="/about-us" component={AboutUsPage}></Route>
-            <Route exact path="/activity" component={ActivityPage}></Route>
-            <Route exact path="/contact" component={ContactPage}></Route>
-            <Route exact path="/" component={MainPage}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </>
+        <Switch>
+          <Route exact path="/dogs" component={DogPage}></Route>
+          <Route exact path="/exco" component={ExcoPage}></Route>
+          <Route exact path="/about-us" component={AboutUsPage}></Route>
+          <Route exact path="/activity" component={ActivityPage}></Route>
+          <Route exact path="/contact" component={ContactPage}></Route>
+          <Route exact path="/" component={MainPage}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
